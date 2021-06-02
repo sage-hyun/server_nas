d85df274f7113e639d9571482e4b8eba59b692f2
sage-cau <dan06052@naver.com>
2021-06-02 11:42:02 +0900

    가족코드 갱신 알고리즘 모두 동일한 현상 해결

--- family_code.js
+++ family_code.js
@@ -8,11 +8,10 @@ const { sequelize } = require("./models");
 
     family_all.forEach(async elem => {
         // 새 가족 코드 생성
-        do {
-            new_family_code = Math.random().toString(36).substr(2,8);
-            var family = await models.family.findOne({where:{family_code:new_family_code}});
-        } while (family)    // 중복 확인
 
-        await models.family.update({family_code: new_family_code}, {where:{family_id:elem.family_id}});
+        await models.family.update({family_code: Math.random().toString(36).substr(2,8)}, 
+        {where:{family_id:elem.family_id}});
+
+
     });
 })();
