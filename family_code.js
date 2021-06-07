00e51a763c2cc68af2201ccb9f449ff4e2e705df
sage-cau <dan06052@naver.com>
2021-06-07 16:56:24 +0900

    family code 재발급 코드 수정

--- family_code.js
+++ family_code.js
@@ -6,12 +6,13 @@ const { sequelize } = require("./models");
 (async () => {
     var family_all = await models.family.findAll({raw: true});
 
-    family_all.forEach(async elem => {
+    for(var i = 0; i < family_all.length; i++) {
         // 새 가족 코드 생성
+        do {
+            new_family_code = Math.random().toString(36).substr(2,8);
+            var family = await models.family.findOne({where:{family_code:new_family_code}});
+        } while (family)    // 중복 확인
 
-        await models.family.update({family_code: Math.random().toString(36).substr(2,8)}, 
-        {where:{family_id:elem.family_id}});
-
-
-    });
+        await models.family.update({family_code: new_family_code}, {where:{family_id:family_all[i].family_id}});
+    };
 })();
